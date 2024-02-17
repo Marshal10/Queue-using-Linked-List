9bbe21c14c30ecb375f2451e67bab09bddb4a84b
Marshal10 <marshalmaleane123@gmail.com>
2024-02-17 17:26:16 +0530

    Dequeue method

--- main.py
+++ main.py
@@ -38,13 +38,25 @@ class Queue:
         if self.linkedList.head is None:
             return True
         return False
+    
+    def dequeue(self):
+        popped_node=self.linkedList.head
+        if self.linkedList.head==self.linkedList.tail:
+            self.linkedList.head=self.linkedList.tail=None
+        else:
+            self.linkedList.head=self.linkedList.head.next
+        return popped_node
         
     
 customQueue=Queue()
-print(customQueue)
-print(customQueue.isEmpty())
 print(customQueue.enqueue(10))
 print(customQueue.enqueue(11))
 print(customQueue.enqueue(12))
-print(customQueue.isEmpty())
 print(customQueue)
+print(customQueue.dequeue())
+print(customQueue)
+print(customQueue.dequeue())
+print(customQueue)
+print(customQueue.dequeue())
+print(customQueue)
+print(customQueue.isEmpty())
