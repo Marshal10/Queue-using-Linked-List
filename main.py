8ff2f21e0c76f20f7ac90da3cb0bb9217f6152d3
Marshal10 <marshalmaleane123@gmail.com>
2024-02-17 17:18:43 +0530

    Iniialize the node,linked list and queue class

---
+++ main.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+    
+    def __str__(self):
+        return str(self.value)
+    
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+        
+    def __iter__(self):
+        curNode=self.head
+        while curNode:
+            yield curNode
+            curNode=curNode.next
+            
+class Queue:
+    def __init__(self):
+        self.linkedList=LinkedList()
+        
+    def __str__(self):
+        values=[str(x) for x in self.linkedList]
+        return ' '.join(values)
+    
+customQueue=Queue()
+print(customQueue)
